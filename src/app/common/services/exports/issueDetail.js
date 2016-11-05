1384c3da605386f2f00a196603b9e177d7e4d536
KyleRiggen <kyle.riggenbach@gmail.com>
2016-11-05 03:21:22 -0600

    issue export more stuff

--- src/app/common/services/exports/issueDetail.js
+++ src/app/common/services/exports/issueDetail.js
@@ -54,7 +54,7 @@
       doc.ellipse(440, 109, 2, 2, 'F');
       doc.ellipse(430, 109, 2, 2, 'F');
       doc.ellipse(420, 109, 2, 2, 'F');
-
+      // center
       doc.ellipse(180, 109, 2, 2, 'F');
       doc.ellipse(170, 109, 2, 2, 'F');
       doc.ellipse(160, 109, 2, 2, 'F');
@@ -72,10 +72,10 @@
       doc.ellipse(40, 109, 2, 2, 'F');
       doc.ellipse(30, 109, 2, 2, 'F');
       doc.ellipse(20, 109, 2, 2, 'F');
+      doc.text(260, 112, "Description");
 
       var descriptionLength = 78;
       var descLeftofPoint = 0;
-      doc.text(260, 112, "Description");
 
       if (data.description.length > descriptionLength) {
         var numTimes = Math.floor(data.description.length / descriptionLength);
@@ -92,12 +92,52 @@
         doc.text(20, 130 + descLeftofPoint, "" + data.description);
       }
 
+      doc.setFillColor(33, 150, 243);
+      doc.roundedRect(
+        188, // x position
+        281, // y position | bigger is lower on page
+        224, // x length
+        18, // y height
+        7, 7, // rounded corners
+        'F'); // F filled | FD filled with borders
 
-      // if due date exists
-      if (data.due) {
-        doc.text(40, 124, "Due date: ");
-        doc.text(200, 124, dueDate);
-      }
+      doc.ellipse(580, 290, 2, 2, 'F');
+      doc.ellipse(570, 290, 2, 2, 'F');
+      doc.ellipse(560, 290, 2, 2, 'F');
+      doc.ellipse(550, 290, 2, 2, 'F');
+      doc.ellipse(540, 290, 2, 2, 'F');
+      doc.ellipse(530, 290, 2, 2, 'F');
+      doc.ellipse(520, 290, 2, 2, 'F');
+      doc.ellipse(510, 290, 2, 2, 'F');
+      doc.ellipse(500, 290, 2, 2, 'F');
+      doc.ellipse(490, 290, 2, 2, 'F');
+      doc.ellipse(480, 290, 2, 2, 'F');
+      doc.ellipse(470, 290, 2, 2, 'F');
+      doc.ellipse(460, 290, 2, 2, 'F');
+      doc.ellipse(450, 290, 2, 2, 'F');
+      doc.ellipse(440, 290, 2, 2, 'F');
+      doc.ellipse(430, 290, 2, 2, 'F');
+      doc.ellipse(420, 290, 2, 2, 'F');
+      // center
+      doc.ellipse(180, 290, 2, 2, 'F');
+      doc.ellipse(170, 290, 2, 2, 'F');
+      doc.ellipse(160, 290, 2, 2, 'F');
+      doc.ellipse(150, 290, 2, 2, 'F');
+      doc.ellipse(140, 290, 2, 2, 'F');
+      doc.ellipse(130, 290, 2, 2, 'F');
+      doc.ellipse(120, 290, 2, 2, 'F');
+      doc.ellipse(110, 290, 2, 2, 'F');
+      doc.ellipse(100, 290, 2, 2, 'F');
+      doc.ellipse(90,  290, 2, 2, 'F');
+      doc.ellipse(80, 290, 2, 2, 'F');
+      doc.ellipse(70, 290, 2, 2, 'F');
+      doc.ellipse(60, 290, 2, 2, 'F');
+      doc.ellipse(50, 290, 2, 2, 'F');
+      doc.ellipse(40, 290, 2, 2, 'F');
+      doc.ellipse(30, 290, 2, 2, 'F');
+      doc.ellipse(20, 290, 2, 2, 'F');
+      doc.text(260, 293, "Comments");
+
 
       var commentLength = 70;
       var leftofPoint = 0;
